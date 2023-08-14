e0f1e36b48403f06dc5f125fa552ec7a38db070f
Yahya Abounoussir <yahya.abounoussir@gmail.com>
2023-08-14 22:30:25 +0100

    C

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -12,7 +12,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *ptr, *end;
 
-	if (head __ NULL)
+	if (head == NULL)
 		return (NULL);
 	for (end = head->next; end != NULL; end = end->next)
 	{
