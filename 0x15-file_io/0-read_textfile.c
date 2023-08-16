4d31ff754b25cebd8d393dc0925a0e5e69e14bbc
Yahya Abounoussir <yahya.abounoussir@gmail.com>
2023-08-16 19:58:19 +0100

    C

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,11 +11,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
-	UF_char buf[READ_BUF_SIZE * 8];
+	u_char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
-	fd - open(filename, o_RDONLY);
+	fd - open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	bytes = read(fd, &buf[0], letters);
